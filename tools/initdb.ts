4ea6220d3f9cee230ffabb685774b147730c8ebc
Jeremy Zongker <jeremy@trilitech.com>
2021-02-02 07:03:02 +0000

    Updated db init script

--- tools/initdb.ts
+++ tools/initdb.ts
@@ -7,10 +7,22 @@ const init = async () => {
   dotenv.config();
   console.log("Connecting");
   Pool.initPool();
-  await DBCreator.init(["Links", "Pages"])
-  await DBCreator.runScript("Services", "./dbScripts/services.mysql", false);
-  await DBCreator.runScript("Settings", "./dbScripts/settings.mysql", false);
+  await DBCreator.init(["Links", "Pages"]);
+
+
+
+  const tables: { title: string, file: string }[] = [
+    { title: "Services", file: "services.mysql" },
+    { title: "Settings", file: "settings.mysql" },
+  ];
+  await initTables("StreamingLive", tables);
 };
+
+const initTables = async (displayName: string, tables: { title: string, file: string }[]) => {
+  console.log("");
+  console.log("SECTION: " + displayName);
+  for (const table of tables) await DBCreator.runScript(table.title, "./tools/dbScripts/" + table.file, false);
+}
 
 init()
   .then(() => { console.log("Database Created"); process.exit(0); })
