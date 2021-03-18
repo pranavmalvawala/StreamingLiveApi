a988e52ee09308a180200f47838259f471d3d462
Yonas Fissha <37205298+YonasFissha@users.noreply.github.com>
2021-03-18 05:35:52 +0300

    Page controller modified to write page into db when storage location is DB (#10)
    * page controller modified to write page into db
    
    * database script updated
    
    * changes after review
    
    * page controller updated

--- src/controllers/PageController.ts
+++ src/controllers/PageController.ts
@@ -1,27 +1,30 @@
-import { httpGet, requestParam, controller } from "inversify-express-utils";
+import { httpGet, requestParam, controller, httpPost } from "inversify-express-utils";
 import express from "express";
 import { SubDomainHelper } from "../helpers"
-import { AwsHelper } from "../helpers"
+import { Page, File } from "../apiBase/models";
 import { PageController as BasePageController } from "../apiBase/controllers";
 
 @controller("/pages")
 export class PageControllerExtended extends BasePageController {
 
-    @httpGet("/write/:id")
-    public async writeToDisk(@requestParam("id") id: string, req: express.Request, res: express.Response): Promise<any> {
+    @httpPost("/write")
+    public async writeToDisk(req: express.Request<{}, {}, Page>, res: express.Response): Promise<any> {
         return this.actionWrapper(req, res, async (au) => {
-            if (process.env.STORAGE_LOCATION !== "S3") return this.denyAccess(["This API is not configured to publish to disk"]);
-            else {
-                const page = await this.baseRepositories.page.loadById(id, au.churchId);
-                if (page.content !== undefined) {
-                    const subDomain = await SubDomainHelper.get(au.churchId);
-                    const wrappedContent = this.wrapContent(subDomain, page.content);
-                    const path = "data/" + subDomain + '/page' + page.id + '.html';
-                    const buffer = Buffer.from(wrappedContent, 'binary');
-                    await AwsHelper.S3Upload(path, "text/html", buffer)
-                }
-                return page;
+            const page: Page = req.body;
+            if (page.content !== undefined) {
+                const subDomain = await SubDomainHelper.get(au.churchId);
+                const wrappedContent = this.wrapContent(subDomain, page.content);
+                const tempBase64 = Buffer.from(wrappedContent, 'binary').toString('base64');
+                const fileId = page?.path?.split('/')[2];
+
+                const newFile: File = { id: fileId, churchId: au.churchId, type: "text/html", content: tempBase64 }
+
+
+                await this.baseRepositories.file.save(newFile).then(file => page.path = "/files/" + file.id);
+
             }
+            return page;
+
         });
     }
 
--- tools/initdb.ts
+++ tools/initdb.ts
@@ -7,7 +7,7 @@ const init = async () => {
   dotenv.config();
   console.log("Connecting");
   Pool.initPool();
-  await DBCreator.init(["Links", "Pages"]);
+  await DBCreator.init(["Links", "Pages", "Files"]);
 
 
 
