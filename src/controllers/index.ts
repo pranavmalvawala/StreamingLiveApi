1be46b6d6c9cd11f6472f83848aa7e1530f7a1aa
Jeremy Zongker <jeremy@trilitech.com>
2021-03-22 02:14:39 -0500

    Disk storage (#14)
    * Updated apibase
    
    * Removed page controller

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,5 +1,4 @@
 export { ChurchController } from "./ChurchController";
 export { PreviewController } from "./PreviewController";
 export { ServiceController } from "./ServiceController";
-export { PageControllerExtended } from "./PageController";
 export { LinkController } from "../apiBase"
