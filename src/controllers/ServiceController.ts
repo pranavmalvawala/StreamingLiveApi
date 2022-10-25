afc8cc4a944631526982a84b387c07ee42f9f111
Jeremy Zongker <jeremy@trilitech.com>
2022-10-25 14:53:48 -0500

    Moved permissions management from messaging api

--- src/controllers/ServiceController.ts
+++ src/controllers/ServiceController.ts
@@ -4,56 +4,67 @@ import { Service } from "../models";
 import { StreamingLiveBaseController } from "./StreamingLiveBaseController";
 import { Permissions } from "../helpers/Permissions";
 import { DateTimeHelper } from '../helpers'
+import { EncryptionHelper } from "../apiBase";
 
 @controller("/services")
 export class ServiceController extends StreamingLiveBaseController {
-    @httpGet("/")
-    public async loadAll(req: express.Request, res: express.Response): Promise<any> {
-        return this.actionWrapper(req, res, async (au) => {
-            const services = await this.repositories.service.loadAll(au.churchId);
-            const promises: Promise<any>[] = []
-            services.forEach((s, index, allServices) => {
-                // Update service time
-                if (s.serviceTime < DateTimeHelper.subtractHoursFromNow(6)) {
-                    if(!s.recurring) {
-                        promises.push(this.repositories.service.delete(s.id, s.churchId));
-                        allServices.splice(index, 1)
-                    } else {
-                        s.serviceTime.setDate(s.serviceTime.getDate() + 7);
-                        promises.push(this.repositories.service.save(s));
-                    }
-                }
-                s.serviceTime.setMinutes(s.serviceTime.getMinutes() - s.timezoneOffset);
-            })
-            await Promise.all(promises);
 
-            return services;
-        });
-    }
+  @httpGet("/:id/hostChat")
+  public async getHostChatRoom(@requestParam("id") id: string, req: express.Request<{}, {}, null>, res: express.Response): Promise<any> {
+    return this.actionWrapper(req, res, async (au) => {
+      let room = "";
+      if (au.checkAccess(Permissions.chat.host)) room = EncryptionHelper.encrypt(id);
+      return { room };
+    });
+  }
 
-    @httpDelete("/:id")
-    public async delete(@requestParam("id") id: string, req: express.Request, res: express.Response): Promise<void> {
-        return this.actionWrapper(req, res, async (au) => {
-            if (!au.checkAccess(Permissions.services.edit)) return this.json({}, 401);
-            else {
-                await this.repositories.service.delete(id, au.churchId);
-                return null;
-            }
-        });
-    }
+  @httpGet("/")
+  public async loadAll(req: express.Request, res: express.Response): Promise<any> {
+    return this.actionWrapper(req, res, async (au) => {
+      const services = await this.repositories.service.loadAll(au.churchId);
+      const promises: Promise<any>[] = []
+      services.forEach((s, index, allServices) => {
+        // Update service time
+        if (s.serviceTime < DateTimeHelper.subtractHoursFromNow(6)) {
+          if (!s.recurring) {
+            promises.push(this.repositories.service.delete(s.id, s.churchId));
+            allServices.splice(index, 1)
+          } else {
+            s.serviceTime.setDate(s.serviceTime.getDate() + 7);
+            promises.push(this.repositories.service.save(s));
+          }
+        }
+        s.serviceTime.setMinutes(s.serviceTime.getMinutes() - s.timezoneOffset);
+      })
+      await Promise.all(promises);
 
-    @httpPost("/")
-    public async save(req: express.Request<{}, {}, Service[]>, res: express.Response): Promise<any> {
-        return this.actionWrapper(req, res, async (au) => {
-            if (!au.checkAccess(Permissions.services.edit)) return this.json({}, 401);
-            else {
-                let services: Service[] = req.body;
-                const promises: Promise<Service>[] = [];
-                services.forEach((service) => { if (service.churchId === au.churchId) promises.push(this.repositories.service.save(service)); });
-                services = await Promise.all(promises);
-                return this.json(services, 200);
-            }
-        });
-    }
+      return services;
+    });
+  }
+
+  @httpDelete("/:id")
+  public async delete(@requestParam("id") id: string, req: express.Request, res: express.Response): Promise<void> {
+    return this.actionWrapper(req, res, async (au) => {
+      if (!au.checkAccess(Permissions.services.edit)) return this.json({}, 401);
+      else {
+        await this.repositories.service.delete(id, au.churchId);
+        return null;
+      }
+    });
+  }
+
+  @httpPost("/")
+  public async save(req: express.Request<{}, {}, Service[]>, res: express.Response): Promise<any> {
+    return this.actionWrapper(req, res, async (au) => {
+      if (!au.checkAccess(Permissions.services.edit)) return this.json({}, 401);
+      else {
+        let services: Service[] = req.body;
+        const promises: Promise<Service>[] = [];
+        services.forEach((service) => { if (service.churchId === au.churchId) promises.push(this.repositories.service.save(service)); });
+        services = await Promise.all(promises);
+        return this.json(services, 200);
+      }
+    });
+  }
 
 }
